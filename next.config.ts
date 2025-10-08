fdbe129c49e7708277a87bcb9c893e6cf50fff02
Makar <makardovgopolji@gmail.com>
2025-10-08 16:55:03 +0200

    initialization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin('./src/lib/i18nRequest.ts');
+
+export default withNextIntl(nextConfig);
